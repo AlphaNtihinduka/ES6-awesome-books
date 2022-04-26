5212bf0e17807ec1fb45b1c0c3c975723de26a39
AlphaNtihinduka <ntihindukaalpha@yahoo.com>
2022-04-26 22:36:11 +0300

    add UI and event listeners

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
 class Book {
     constructor(title, author){
         this.title = title;
-        this.author = author
+        this.author = author;
     }
 }
 
@@ -17,38 +17,46 @@ class UI {
             },
 
             {
-                title: "The amin in operation",
+                title: "The yu",
                 author: "Mk",
-            }
+            },
+
+            {
+                title: "The amin",
+                author: "Mk",
+            },
 
         ];
 
     const books = booksStore;
-
     books.forEach((book) => UI.addBookToList(book));
     }
 
-    static addBookToList(){
-        const booKList = document.querySelector(".book-list");
-        const bookDetails = document.querySelector(".book-details");
-        const title = document.querySelector(".title");
-        const author = document.querySelector(".author");
-        const deleteBtn = document.querySelector(".delete");
-        title.textContent = `
-            ${book.title};`;
+    static addBookToList(book){
+        const bookList = document.querySelector(".book-list");
+        const bookDetails = document.createElement("div");
 
-        author.textContent = `
-        ${book.author}`
+        bookDetails.innerHTML = `
+            <div class ="book-details">
+            <span>"${book.title}" by </span>
+            <span>${book.author}</span>
+            <span><a href='#' class="remove-btn">Remove</a></span>
+            <div>
+        `
+        bookList.appendChild(bookDetails);
+    }
 
-        author.textContent = `
-        ${book.author}`;
+    static deleteBook(removed){
+            if(removed.classList.contains = "remove-btn") {
+                removed.parentElement.parentElement.remove();
+            }
+    }
 
-        deleteBtn.textContent = `Remove`;
-
-        
+    static cleaFields(){
+        document.querySelector(".title").value = "";
+        document.querySelector(".author").value = "";
     }
 }
-
 
 //store class: Handles the local storage.
 
@@ -56,5 +64,31 @@ class UI {
 document.addEventListener('DOMContentLoaded', UI.displayBooks);
 
 //Event: Add book
+bookForm = document.querySelector(".books-form");
+bookForm.addEventListener("submit", (e)=>{
+    //Grabbing form values
+    const title = document.querySelector(".title").value;
+    const author = document.querySelector(".author").value;
+
+    if(title === "" || author === ""){
+        
+    }else{
+     // instatiate book
+     const book = new Book(title, author);
+    
+     //add book to UI
+     UI.addBookToList(book);
+ 
+     //clear fields
+     UI.cleaFields()
+ 
+     //Prevent page reload
+     e.preventDefault();
+    }
+
+})
 
 //Event: Remove book
+document.querySelector(".book-list").addEventListener("click", (e)=>{
+    UI.deleteBook(e.target);
+})
