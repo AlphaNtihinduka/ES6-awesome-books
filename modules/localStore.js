ab7086e0039a24d934cb450e074033306d58f892
AlphaNtihinduka <ntihindukaalpha@yahoo.com>
2022-04-29 04:35:19 +0300

    Modify the local remove method

--- modules/localStore.js
+++ modules/localStore.js
@@ -1,6 +1,6 @@
 class BookLocalStore {
   static getBooks() {
-    const books = JSON.parse(localStorage.getItem('books')) || []
+    const books = JSON.parse(localStorage.getItem('books')) || [];
     return books;
   }
 
@@ -10,27 +10,12 @@ class BookLocalStore {
     localStorage.setItem('books', JSON.stringify(books));
   }
 
-  // static removeBook(tar) {
-  //   console.log(tar)
-  //   let books = BookLocalStore.getBooks();
-  //   let index = books.findIndex((item) => item.title === tar.title && item.author === tar.author);
-  
-  //   books.splice(index, 1);
-  //   localStorage.setItem("books", JSON.stringify(books)); 
-  // }
   static removeBook(tar) {
-        const books = BookLocalStore.getBooks();
-        let found = null;
-        books.forEach((book, index) => {
-          if(book.title ===tar.title && book.author === tar.author) {
-            found = index;
-          }         
-        });
-          if(found != null) {
-            books.splice(found, 1)
-          }
-         localStorage.setItem("books", JSON.stringify(books));
-     }
+    const books = BookLocalStore.getBooks();
+    const found = books.findIndex((book) => book.title === tar.title && book.author === tar.author);
+    books.splice(found, 1);
+    localStorage.setItem('books', JSON.stringify(books));
+  }
 }
 
 export default BookLocalStore;
